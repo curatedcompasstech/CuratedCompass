0c934d6df4703c89b1476c4101802f4501903db6
Rameez Khazi <rameezkhazi@Rameezs-MacBook-Pro.local>
2025-04-29 12:57:46 +0530

    Resolve CORS

--- bff/src/main.ts
+++ bff/src/main.ts
@@ -5,7 +5,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:8080', 'https://curatedcompass.in'],
+    origin: [
+      'http://localhost:8080',
+      'https://curatedcompass.in',
+      'https://curatedcompass.github.io',
+    ],
     credentials: true,
   });
 
